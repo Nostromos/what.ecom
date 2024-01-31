97eb8a3c81d3993a494c5a885d6ec409012be60f
Michael Monaghan <Nostromos@users.noreply.github.com>
2024-01-31 14:02:24 -0500

    Starting to flesh out a list of basic routes

---
+++ backend/routes/ProductRouter.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const ProductRouter = express.Router();
+
+// GET All Products
+
+// GET A Single Product by ID
+
+// GET A list of products by category
+
+// POST A Single Product
+
+// POST A List of Products
+
+// PUT Update a single product by ID
+
+// PUT Update a list of products 
+
+// DELETE A single product by ID
+
+// DELETE A list of products by ID
