0ffe74438ea02694aea30b16b700565d47dc7e05
Michael Monaghan <Nostromos@users.noreply.github.com>
2024-02-02 15:58:39 -0500

    Component skeleton structure setup with placeholder copy and functionality.

---
+++ frontend/src/components/CurrentDeals/CurrentDeals.jsx
@@ -0,0 +1,14 @@
+/**
+ * Links to current items on sale/deal.
+ * 
+ * @returns {ReactNode} Returns a component that displays whatever the current deal splash is and a button that links to a filtered list of products for those sale items.
+ */
+
+export default function CurrentDeals() {
+  
+  return (
+    <section>
+      List of product cards...
+    </section>
+  )
+}
---
+++ frontend/src/components/HolidayDeal/HolidayDeal.jsx
@@ -0,0 +1,15 @@
+/**
+ * Component to display the current campaign/deal on the hompage. Should link to a selection of products included in that deal.
+ * 
+ * @returns {React.Component} Returns the holiday deal/campaign component.
+ */
+
+export default function HolidayDeal() {
+  
+  return (
+    <section>
+      <p>Holiday Deal!</p>
+      <button>BUY NOW</button>
+    </section>
+  )
+}
