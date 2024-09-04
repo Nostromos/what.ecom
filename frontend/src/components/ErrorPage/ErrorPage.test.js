5725a215cb9e28a1fbd68077f7b7ab0040292962
Michael Monaghan <Nostromos@users.noreply.github.com>
2024-09-04 12:37:47 -0400

    Long awaited updates. Still working on adding react-router but will likely come back to this in a few weeks after I complete a couple other side projects that have been languishing.

--- frontend/src/components/ErrorPage/ErrorPage.test.js
+++ frontend/src/components/ErrorPage/ErrorPage.test.js
@@ -1,18 +1,18 @@
-import { useRouteError } from "react-router-dom";
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import '@testing-library/jest-dom'
+import Fetch from './fetch'
+import { test, expect } from '@jest/globals'
 
-function ErrorPage() {
-  const error = useRouteError();
-  console.error(error);
+test('loads and displays error', async () => {
+  // ARRANGE
+  render(<Fetch url="/error" />);
 
-  return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-    </div>
-  )
-}
+  // ACT
+  await userEvent.click(screen.getByText('Load error'));
+  await screen.findByRole('heading');
 
-export default ErrorPage
+  // ASSERT
+  expect(screen.getByRole('heading')).toHaveTextContext('Sorry, an unexpected error has occurred.');
+  expect(screen.getByRole('button')).toBeDisabled();
+});
